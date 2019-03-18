a2127d6301844dcee2de288796402f5676673a14
enriquefdez5 <uo257742@uniovi.es>
2019-03-18 16:06:54 +0100

    SDI-NodeJS- 11,12,13,14 Vistas y motores de plantillas

--- app.js
+++ app.js
@@ -9,10 +9,12 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+var swig = require('swig');
+
 
 //Rutas/controladores por lógica
-require("./routes/rusuarios.js")(app); // (app, param1, param2, etc.)
-require("./routes/rcanciones.js")(app); // (app, param1, param2, etc.)
+require("./routes/rusuarios.js")(app,swig); // (app, param1, param2, etc.)
+require("./routes/rcanciones.js")(app, swig); // (app, param1, param2, etc.)
 
 
 // lanzar el servidor
