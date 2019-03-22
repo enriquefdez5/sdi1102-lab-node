af830585832e4fb0b436fb68aa1810c5736d676d
enriquefdez5 <uo257742@uniovi.es>
2019-03-22 17:35:10 +0100

    SDI-NodeJS-8.3 - Arquitectura para el acceso a datos

--- app.js
+++ app.js
@@ -14,13 +14,16 @@ app.set('port', 8081);
 app.set('db', 'mongodb://admin:Caquita05.@tiendamusica-shard-00-00-ntnag.mongodb.net:27017,tiendamusica-shard-00-01-ntnag.mongodb.net:27017,tiendamusica-shard-00-02-ntnag.mongodb.net:27017/test?ssl=true&replicaSet=tiendamusica-shard-0&authSource=admin&retryWrites=true');
 app.use(express.static('public'));
 
+//Inicializamos el gestor de BD que usará MongoDB
+var gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app,mongo);
 
 
 
 var comentarios = [];
 //Rutas/controladores por lógica
-require("./routes/rusuarios.js")(app, swig); // (app, param1, param2, etc.)
-require("./routes/rcanciones.js")(app, swig, mongo); // (app, param1, param2, etc.)
+require("./routes/rusuarios.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
+require("./routes/rcanciones.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
 
 
 // lanzar el servidor
